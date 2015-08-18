616d470e48b868df406d16f988f628a5cbe246c1
HenryLee <henrylee_cn@foxmail.com>
2015-08-18 10:14:48 +0800

    扩展TeleportAPI

--- node/client_api.go
+++ node/client_api.go
@@ -10,28 +10,38 @@ import (
 func ClientApi(n *Node) API {
 	return API{
 		// 接收来自服务器的任务并加入任务库
-		"task": func(receive *NetData) *NetData {
-			d, err := json.Marshal(receive.Body)
-			if err != nil {
-				log.Println("json编码失败", receive.Body)
-				return nil
-			}
-			t := &task.Task{}
-			err = json.Unmarshal(d, t)
-			if err != nil {
-				log.Println("json解码失败", receive.Body)
-				return nil
-			}
-			n.TaskJar.Into(t)
-			return nil
-		},
+		"task": &task2{n},
 
 		// 打印接收到的报告
-		"log": func(receive *NetData) *NetData {
-			log.Printf(" * ")
-			log.Printf(" *     [ %s ]    %s", receive.From, receive.Body)
-			log.Printf(" * ")
-			return nil
-		},
+		"log": new(log2),
 	}
+}
+
+type task2 struct {
+	*Node
+}
+
+func (self *task2) Process(receive *NetData) *NetData {
+	d, err := json.Marshal(receive.Body)
+	if err != nil {
+		log.Println("json编码失败", receive.Body)
+		return nil
+	}
+	t := &task.Task{}
+	err = json.Unmarshal(d, t)
+	if err != nil {
+		log.Println("json解码失败", receive.Body)
+		return nil
+	}
+	self.TaskJar.Into(t)
+	return nil
+}
+
+type log2 struct{}
+
+func (*log2) Process(receive *NetData) *NetData {
+	log.Printf(" * ")
+	log.Printf(" *     [ %s ]    %s", receive.From, receive.Body)
+	log.Printf(" * ")
+	return nil
 }
--- node/server_api.go
+++ node/server_api.go
@@ -8,16 +8,26 @@ import (
 func ServerApi(n *Node) API {
 	return API{
 		// 提供任务给客户端
-		"task": func(receive *NetData) *NetData {
-			return ReturnData(n.Out(n.CountNodes()))
-		},
+		"task": &task1{n},
 
 		// 打印接收到的报告
-		"log": func(receive *NetData) *NetData {
-			log.Printf(" * ")
-			log.Printf(" *     [ %s ]    %s", receive.From, receive.Body)
-			log.Printf(" * ")
-			return nil
-		},
+		"log": new(log1),
 	}
+}
+
+type task1 struct {
+	*Node
+}
+
+func (self *task1) Process(receive *NetData) *NetData {
+	return ReturnData(self.Out(self.CountNodes()))
+}
+
+type log1 struct{}
+
+func (*log1) Process(receive *NetData) *NetData {
+	log.Printf(" * ")
+	log.Printf(" *     [ %s ]    %s", receive.From, receive.Body)
+	log.Printf(" * ")
+	return nil
 }
