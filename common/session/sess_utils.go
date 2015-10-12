28f83ab74f0c7406fc36f7cf7d995e055d2dbd2a
HenryLee <henrylee_cn@foxmail.com>
2015-10-12 17:15:20 +0800

    解除session对外部包的依赖

--- common/session/sess_utils.go
+++ common/session/sess_utils.go
@@ -26,10 +26,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	r "math/rand"
 	"strconv"
 	"time"
-
-	"github.com/astaxie/beego/utils"
 )
 
 func init() {
@@ -71,9 +70,36 @@ func DecodeGob(encoded []byte) (map[interface{}]interface{}, error) {
 func generateRandomKey(strength int) []byte {
 	k := make([]byte, strength)
 	if n, err := io.ReadFull(rand.Reader, k); n != strength || err != nil {
-		return utils.RandomCreateBytes(strength)
+		return randomCreateBytes(strength)
 	}
 	return k
+}
+
+// RandomCreateBytes generate random []byte by specify chars.
+func randomCreateBytes(n int, alphabets ...byte) []byte {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	var randby bool
+	if num, err := rand.Read(bytes); num != n || err != nil {
+		r.Seed(time.Now().UnixNano())
+		randby = true
+	}
+	for i, b := range bytes {
+		if len(alphabets) == 0 {
+			if randby {
+				bytes[i] = alphanum[r.Intn(len(alphanum))]
+			} else {
+				bytes[i] = alphanum[b%byte(len(alphanum))]
+			}
+		} else {
+			if randby {
+				bytes[i] = alphabets[r.Intn(len(alphabets))]
+			} else {
+				bytes[i] = alphabets[b%byte(len(alphabets))]
+			}
+		}
+	}
+	return bytes
 }
 
 // Encryption -----------------------------------------------------------------
--- common/util/util.go
+++ common/util/util.go
@@ -3,6 +3,7 @@ package util
 
 import (
 	"crypto/md5"
+	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
 	"encoding/xml"
@@ -12,10 +13,12 @@ import (
 	"hash/fnv"
 	"io"
 	"log"
+	r "math/rand"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // JsonpToJson modify jsonp string to json string
@@ -220,4 +223,31 @@ func Atoui(str interface{}) uint {
 	}
 	u, _ := strconv.Atoi(strings.Trim(str.(string), " "))
 	return uint(u)
+}
+
+// RandomCreateBytes generate random []byte by specify chars.
+func RandomCreateBytes(n int, alphabets ...byte) []byte {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	var randby bool
+	if num, err := rand.Read(bytes); num != n || err != nil {
+		r.Seed(time.Now().UnixNano())
+		randby = true
+	}
+	for i, b := range bytes {
+		if len(alphabets) == 0 {
+			if randby {
+				bytes[i] = alphanum[r.Intn(len(alphanum))]
+			} else {
+				bytes[i] = alphanum[b%byte(len(alphanum))]
+			}
+		} else {
+			if randby {
+				bytes[i] = alphabets[r.Intn(len(alphabets))]
+			} else {
+				bytes[i] = alphabets[b%byte(len(alphabets))]
+			}
+		}
+	}
+	return bytes
 }
