6a86f7b4e2506528afcd97969bede2174a818a53
HenryLee <henrylee_cn@foxmail.com>
2016-03-24 17:12:27 +0800

    update: cmd服务端模式支持连续添加任务

--- cmd/pholcus-cmd.go
+++ cmd/pholcus-cmd.go
@@ -5,11 +5,13 @@ package cmd
 
 import (
 	"flag"
+	"fmt"
 	"strconv"
 	"strings"
 
 	"github.com/henrylee2cn/pholcus/app"
 	"github.com/henrylee2cn/pholcus/app/spider"
+	"github.com/henrylee2cn/pholcus/logs"
 	"github.com/henrylee2cn/pholcus/runtime/cache"
 	"github.com/henrylee2cn/pholcus/runtime/status"
 )
@@ -46,10 +48,27 @@ func Flag() {
 // 执行入口
 func Run() {
 	app.LogicApp.Init(cache.Task.Mode, cache.Task.Port, cache.Task.Master)
+	if cache.Task.Mode == status.UNSET {
+		return
+	}
+	switch app.LogicApp.GetAppConf("Mode").(int) {
+	case status.SERVER:
+		for {
+			parseInput()
+			run()
+		}
+	case status.CLIENT:
+		run()
+		select {}
+	default:
+		run()
+	}
+}
 
+// 运行
+func run() {
 	// 创建蜘蛛队列
 	sps := []*spider.Spider{}
-
 	for _, idx := range strings.Split(*spiderflag, ",") {
 		idx = strings.TrimSpace(idx)
 		if idx == "" {
@@ -59,12 +78,97 @@ func Run() {
 		sps = append(sps, app.LogicApp.GetSpiderLib()[i])
 	}
 
-	// 运行
 	app.LogicApp.SpiderPrepare(sps).Run()
+}
 
-	if app.LogicApp.GetAppConf("Mode").(int) <= status.OFFLINE {
-		return
+// 服务器模式下接收添加任务的参数
+func parseInput() {
+	logs.Log.Informational("\n添加任务参数——必填：%v\n添加任务参数——必填可选：%v\n", "-c_spider", []string{
+		"-a_keyins",
+		"-a_limit",
+		"-a_outtype",
+		"-a_thread",
+		"-a_pause",
+		"-a_proxyminute",
+		"-a_dockercap",
+		"-a_success",
+		"-a_failure"})
+	logs.Log.Informational("\n添加任务：\n")
+retry:
+	*spiderflag = ""
+	input := [12]string{}
+	fmt.Scanln(&input[0], &input[1], &input[2], &input[3], &input[4], &input[5], &input[6], &input[7], &input[8], &input[9])
+	if strings.Index(input[0], "=") < 4 {
+		logs.Log.Informational("\n添加任务的参数不正确，请重新输入：")
+		goto retry
 	}
-
-	select {}
+	for _, v := range input {
+		i := strings.Index(v, "=")
+		if i < 4 {
+			continue
+		}
+		key, value := v[:i], v[i+1:]
+		switch key {
+		case "-a_keyins":
+			cache.Task.Keyins = value
+		case "-a_limit":
+			limit, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				break
+			}
+			cache.Task.Limit = limit
+		case "-a_outtype":
+			cache.Task.OutType = value
+		case "-a_thread":
+			thread, err := strconv.Atoi(value)
+			if err != nil {
+				break
+			}
+			cache.Task.ThreadNum = thread
+		case "-a_pause":
+			pause, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				break
+			}
+			cache.Task.Pausetime = pause
+		case "-a_proxyminute":
+			proxyminute, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				break
+			}
+			cache.Task.ProxyMinute = proxyminute
+		case "-a_dockercap":
+			dockercap, err := strconv.Atoi(value)
+			if err != nil {
+				break
+			}
+			cache.Task.DockerCap = dockercap
+		case "-a_success":
+			if value == "true" {
+				cache.Task.SuccessInherit = true
+			} else if value == "false" {
+				cache.Task.SuccessInherit = false
+			}
+		case "-a_failure":
+			if value == "true" {
+				cache.Task.FailureInherit = true
+			} else if value == "false" {
+				cache.Task.FailureInherit = false
+			}
+		case "-c_spider":
+			*spiderflag = value
+		default:
+			logs.Log.Informational("\n不可含有未知参数，必填参数：%v\n可选参数：%v\n", "-c_spider", []string{
+				"-a_keyins",
+				"-a_limit",
+				"-a_outtype",
+				"-a_thread",
+				"-a_pause",
+				"-a_proxyminute",
+				"-a_dockercap",
+				"-a_success",
+				"-a_failure"})
+			goto retry
+		}
+	}
 }
